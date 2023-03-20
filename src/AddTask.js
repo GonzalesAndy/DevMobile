141dc44009a18bfd29e2c557dcb3426de5d73bb7
GonzalesAndy <andy.gonzales@etu.univ-amu.fr>
2023-03-20 18:44:15 +0100

    Supression et ajout marche (mais ajout doit reload la page pour etre visible)

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,18 +1,51 @@
 import React, { Component } from "react";
+import LocalStorage from "./LocalStorage";
 
 class AddTaskModal extends Component {
+
+  constructor(props) {
+    const localStorage = new LocalStorage();
+    super(props);
+    this.state = {
+      taskName: "",
+      tasks: localStorage.getTasks()
+    };
+    this.handleTaskNameChange = this.handleTaskNameChange.bind(this);
+    this.handleAddTask = this.handleAddTask.bind(this);
+  }
+
+  handleTaskNameChange(event) {
+    this.setState({ taskName: event.target.value });
+    console.log(this.state.taskName);
+  }
+
+  handleAddTask() {
+    const tasks = [...this.state.tasks];
+    tasks.push({ title: this.state.taskName, isChecked: false });
+    const localStorage = new LocalStorage();
+    localStorage.saveTasks(tasks);
+    //re render task List
+    this.setState({ tasks: tasks });
+    this.props.onClose();
+    window.location.reload();
+  }
+
   render() {
 
     const { isOpen, onClose } = this.props;
     return isOpen ? (
-      <div className="modal absolute bg-rose-800 inset-x-0 m-5 p-3">
-        <div className="modal-content">
-          <span className="close" onClick={onClose}>
-            &times;
+      <div className="modal absolute inset-x-0 bottom-[170px] m-5 p-3 pt-10 text-white bg-neutral-600 border border-2 border-black rounded-xl">
+        <div className="flex flex-col">
+          <span className="text-red-500 close absolute top-2" onClick={onClose}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+            </svg>
           </span>
-          <h2>Add Task</h2>
-          <input type="text" placeholder="Enter task name" />
-          <button>Add</button>
+          <h2 className="mb-2">Ajouter une tâche</h2>
+          <input className="inputName text-black" type="text" placeholder=" Entre le nom de la tâche" value={this.state.taskName} onChange={this.handleTaskNameChange}/>
+          <button className="bg-green-500 p-2 rounded-xl mt-2" onClick={this.handleAddTask}>Ajouter</button>
+
         </div>
       </div>
     ) : null;
--- src/TaskList.js
+++ src/TaskList.js
@@ -11,6 +11,7 @@ class TaskList extends Component {
         this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
         this.moveTaskUp = this.moveTaskUp.bind(this);
         this.moveTaskDown = this.moveTaskDown.bind(this);
+        this.deleteTask = this.deleteTask.bind(this);
     }
 
     handleCheckboxChange(event, index, changedTaskTitle) {
@@ -26,6 +27,17 @@ class TaskList extends Component {
     
         const checkedTasksCount = updatedTasks.filter((task) => task.isChecked).length;
         this.props.updateCheckedTasks(Math.round(checkedTasksCount/updatedTasks.length*100));
+    }
+
+    deleteTask(index) {
+        const localStorage = new LocalStorage();
+        const tasks = localStorage.getTasks();
+        tasks.splice(index, 1);
+        localStorage.saveTasks(tasks);
+        //change number of checked tasks
+        const checkedTasksCount = tasks.filter((task) => task.isChecked).length;
+        this.props.updateCheckedTasks(Math.round(checkedTasksCount/tasks.length*100));
+        this.setState({ tasks: tasks });
     }
 
     moveTaskUp(index) {
@@ -55,7 +67,6 @@ class TaskList extends Component {
         const filteredTasks = tasks.filter(task =>
             task.title.toLowerCase().includes(searchText.toLowerCase())
           );
-        
         const taskList = filteredTasks.map((task, index) => (
             <li className="flex flex-row justify-between items-center text-left bg-neutral-500 m-2 p-1 pl-4 border border-2 border-black rounded-xl" key={index}>
                 <div>
@@ -65,13 +76,19 @@ class TaskList extends Component {
                 {searchText === "" && (
                 <div>
                 <button className="bg-neutral-500  p-1 pl-2 pr-2 border border-2 border-black rounded-xl" onClick={(event) => this.moveTaskUp(index)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-up" viewBox="0 0 16 16">
                         <path fillRule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
                     </svg>
                 </button>
                 <button className="bg-neutral-500  p-1 pl-2 pr-2 border border-2 border-black rounded-xl" onClick={(event) => this.moveTaskDown(index)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-down" viewBox="0 0 16 16">
                         <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
+                    </svg>
+                </button>
+                <button className="text-red-500 bg-neutral-500  p-1 pl-2 pr-2 border border-2 border-black rounded-xl" onClick={(event) => this.deleteTask(index)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                     </svg>
                 </button>
                 </div>
